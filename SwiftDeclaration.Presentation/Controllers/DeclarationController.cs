b8b0e6494053a2aeb15b391ea493451cda03565f
Sandro Revazishvili <sandro_revazishvili@epam.com>
2024-01-19 06:10:21 +0400

    Add ability to get list of declarations

---
+++ SwiftDeclaration.Presentation/Controllers/DeclarationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwiftDeclaration.Application.Declarations.Commands.AddDeclaration;
+using SwiftDeclaration.Application.Declarations.Queries.GetAllDeclarationsBriefDetails;
+using SwiftDeclaration.Infrastructure.Models;
+using SwiftDeclaration.Presentation.Services;
+
+namespace SwiftDeclaration.Presentation.Controllers;
+public class DeclarationController : Controller
+{
+    private readonly IHttpClientService _httpClientService;
+
+    public DeclarationController(IHttpClientService httpClientService)
+    {
+        _httpClientService = httpClientService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var result = await _httpClientService
+            .GetAsync<HttpResultBase<List<GetAllDeclarationsBriefDetailsQueryResponse>>>("api/v1/declarations/brief-details");
+
+        return View(result.Payload);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDeclaration(AddDeclarationCommand command)
+    {
+
+        return View();
+    }
+
+}
---
+++ SwiftDeclaration.Presentation/Services/HttpClientService.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace SwiftDeclaration.Presentation.Services;
+
+public interface IHttpClientService
+{
+    Task<TResponse> GetAsync<TResponse>(string url);
+    Task<TResponse> PostAsync<TResponse>(string url, object data);
+    Task PostAsync(string url, object data);
+}
+
+public class HttpClientService : IHttpClientService
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpClientService(IConfiguration configuration)
+    {
+        _httpClient = new()
+        {
+            BaseAddress = new Uri(configuration.GetSection("SwiftDeclarationApi").Value!)
+        };
+    }
+
+    public async Task<TResponse> GetAsync<TResponse>(string url)
+    {
+        var response = await _httpClient.GetAsync($"{_httpClient.BaseAddress}{url}");
+        var data = await response.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<TResponse>(data)
+            ?? throw new Exception("Can't deserialize object"); ;
+    }
+
+    public async Task<TResponse> PostAsync<TResponse>(string url, object data)
+    {
+        var content = await SendPostRequestAsync(url, data);
+
+        return JsonConvert.DeserializeObject<TResponse>(content)
+            ?? throw new Exception("Can't deserialize object"); ;
+    }
+
+    public async Task PostAsync(string url, object data)
+    {
+        await SendPostRequestAsync(url, data);
+    }
+
+    private async Task<string> SendPostRequestAsync(string url, object data)
+    {
+        var content = SerializeObject(data);
+        var response = await _httpClient.PostAsync(url, content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(response.ReasonPhrase);
+        }
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private static StringContent SerializeObject(object data)
+        => new(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+}
